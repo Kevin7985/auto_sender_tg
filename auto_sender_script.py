8b0c71d09838b9ef179aff1be40e4ca5092947a1
Kevin7985 <1298282@mail.ru>
2022-01-13 01:22:07 +0300

    last message getter && writing steps

--- auto_sender_script.py
+++ auto_sender_script.py
@@ -1,15 +1,66 @@
 from pyrogram import Client
 import csv
+from time import sleep
 
 from config import *
 
 username = "username"
 chat = "chat title / username"
 
+def lastChatMessage(app, chatID):
+    for dialog in app.iter_dialogs():
+        if (dialog.chat.id == chatID):
+            return dialog.top_message.text
+
+f = open("input.csv", "r", encoding = "utf-8")
+reader = csv.reader(f)
 with (Client(username, apiID, apiHash)) as app:
     chatID = None
     for dialog in app.iter_dialogs():
         if (dialog.chat.username == chat):
             chatID = dialog.chat.id
             break
-    print(chatID)
+
+    header = False
+    am = 0
+    for item in reader:
+        am += 1
+        if (not header) or am < 32:
+            header = True
+            continue
+
+        #### Your writing steps if you write to bot ####
+
+        # app.send_message(chatID, "/start")
+        # sleep(2)
+        # app.send_message(chatID, item[0])
+        # sleep(2)
+        # app.send_message(chatID, item[1])
+        # sleep(2)
+        # app.send_message(chatID, item[2])
+        # ans = lastChatMessage(app, chatID)
+        # if (ans != "Предмет"):
+        #     inp = input("Correct? (Just press Enter): ")
+        # sleep(1)
+        # app.send_message(chatID, item[3])
+        # ans = lastChatMessage(app, chatID)
+        # if (ans != "Преподаватель"):
+        #     inp = input("Correct? (Just press Enter): ")
+        # sleep(1)
+        # app.send_message(chatID, item[4])
+        # ans = lastChatMessage(app, chatID)
+        # if (ans != "Ссылка vk (личная)"):
+        #     inp = input("Correct? (Just press Enter): ")
+        # sleep(1)
+        # app.send_message(chatID, item[5])
+        # sleep(2)
+        # app.send_message(chatID, item[6])
+        # sleep(2)
+        # app.send_message(chatID, item[7])
+        # sleep(2)
+        # app.send_message(chatID, item[8])
+        # sleep(2)
+        # app.send_message(chatID, item[9])
+        # sleep(2)
+        # app.send_message(chatID, item[10])
+        # sleep(6)
